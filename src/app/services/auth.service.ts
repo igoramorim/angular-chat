1c9136a6ad2e8baca523e233f5a15e57d6b87d52
Igor Amorim <resource245>
2019-09-03 17:23:22 -0300

    inicio da autenticacao 42:40

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private user: Observable<firebase.User>;
+  private authState: any;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFireDatabase,
+    private router: Router
+  ) {
+    this.user = this.afAuth.authState;
+  }
+
+  get currentUserId(): string {
+    return this.authState !== null ? this.authState.uid : '';
+  }
+
+  signUp(email: string, password: string, displayName: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        this.authState = user;
+        const status = 'online';
+        this.setUserData(email, displayName, status);
+      })
+      .catch(error => console.log(error));
+  }
+
+  setUserData(email: string, displayName: string, status: string): void {
+    const path = `users/${this.currentUserId}`;
+    const data = {
+      email: email,
+      displayName: displayName,
+      status: status
+    };
+
+    this.db.object(path).update(data)
+      .catch(error => console.log(error));
+  }
 }
