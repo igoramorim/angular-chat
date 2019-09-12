f405679e1356b1a5ae2ae276147384e799971aef
Igor Amorim <resource245>
2019-09-12 09:36:24 -0300

    diferentes chatrooms

--- src/app/iopage/iopage.component.ts
+++ src/app/iopage/iopage.component.ts
@@ -6,11 +6,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './iopage.component.html',
   styleUrls: ['./iopage.component.css']
 })
-export class IopageComponent implements OnInit {
+export class IopageComponent {
 
-  constructor(private ioService: ChatioService) { }
+  user: string;
+  room: string;
+  messageText: string;
+  messageArray: Array<{ user: string, message: string }> = [];
 
-  ngOnInit() {
+  constructor(private ioService: ChatioService) {
+    this.ioService.newUserJoined()
+      .subscribe((data) => {
+        this.messageArray.push(data);
+      });
+
+    this.ioService.userLeftRoom()
+      .subscribe((data) => {
+        this.messageArray.push(data);
+      });
+
+    this.ioService.newMessageReceived()
+      .subscribe((data) => {
+        this.messageArray.push(data);
+      });
+  }
+
+  join() {
+    this.ioService.joinRoom({ user: this.user, room: this.room });
+  }
+
+  leave() {
+    this.ioService.leaveRoom({ user: this.user, room: this.room });
+  }
+
+  sendMessage() {
+    this.ioService.sendMessage({ user: this.user, room: this.room, message: this.messageText });
   }
 
 }
