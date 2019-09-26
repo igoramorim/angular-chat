e71a9a32f142ce162e017b76918321c862d35856
Igor Amorim <resource245>
2019-09-26 10:01:39 -0300

    passando eventos do index.js para o chat-server.ts

--- server/src/chat-server.ts
+++ server/src/chat-server.ts
@@ -5,55 +5,64 @@ import * as socketIo from 'socket.io';
 import { Message } from './model';
 
 export class ChatServer {
-    public static readonly PORT: number = 3030;
-    private app: express.Application;
-    private server: Server;
-    private io: SocketIO.Server;
-    private port: string | number;
+  public static readonly PORT: number = 3000;
+  private app: express.Application;
+  private server: Server;
+  private io: SocketIO.Server;
+  private port: string | number;
 
-    constructor() {
-        this.createApp();
-        this.config();
-        this.createServer();
-        this.sockets();
-        this.listen();
-    }
+  constructor() {
+    this.createApp();
+    this.config();
+    this.createServer();
+    this.sockets();
+    this.listen();
+  }
 
-    private createApp(): void {
-        this.app = express();
-    }
+  private createApp(): void {
+    this.app = express();
+  }
 
-    private createServer(): void {
-        this.server = createServer(this.app);
-    }
+  private createServer(): void {
+    this.server = createServer(this.app);
+  }
 
-    private config(): void {
-        this.port = process.env.PORT || ChatServer.PORT;
-    }
+  private config(): void {
+    this.port = process.env.PORT || ChatServer.PORT;
+  }
 
-    private sockets(): void {
-        this.io = socketIo(this.server);
-    }
+  private sockets(): void {
+    this.io = socketIo(this.server);
+  }
 
-    private listen(): void {
-        this.server.listen(this.port, () => {
-            console.log('Running server on port %s', this.port);
-        });
+  private listen(): void {
+    this.server.listen(this.port, () => {
+      console.log(`Running server on port ${this.port}`);
+    });
 
-        this.io.on('connect', (socket: any) => {
-            console.log('Connected client on port %s.', this.port);
-            socket.on('message', (m: Message) => {
-                console.log('[server](message): %s', JSON.stringify(m));
-                this.io.emit('message', m);
-            });
+    this.io.on('connection', (socket: any) => {
+      console.log(`[server] Connected client on port ${this.port}`);
 
-            socket.on('disconnect', () => {
-                console.log('Client disconnected');
-            });
-        });
-    }
+      socket.on('join', (data: any) => {
+        socket.join(data.room);
+        console.log(`[server] ${data.user} joined the room ${data.room}`);
+        socket.broadcast.to(data.room).emit('new-user-joined', { user: data.user, message: 'has joined this room.' });
+      });
 
-    public getApp(): express.Application {
-        return this.app;
-    }
+      socket.on('message', (data: any) => {
+        console.log(`[server] message from ${data.user} to room ${data.room} => ${data.message}`);
+        this.io.in(data.room).emit('new-message', { user: data.user, message: data.message });
+      });
+
+      socket.on('leave', (data: any) => {
+        console.log(`[server] ${data.user} left the room ${data.room}`);
+        socket.broadcast.to(data.room).emit('left-room', { user: data.user, message: 'has left this room.' });
+        socket.leave(data.room);
+      });
+    });
+  }
+
+  public getApp(): express.Application {
+    return this.app;
+  }
 }
